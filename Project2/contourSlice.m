9f258878878f39cca585d933c2f5e816e08d7f4e
sindre <sindre.grostad@icloud.com>
2018-11-06 15:44:15 +0100

    contourSlice

---
+++ Project2/contourSlice.m
@@ -0,0 +1,20 @@
+function contourSlice(P,U,vsurf, vrod)
+    % Removing boundary:
+    pTemp = P;
+    pTemp(vsurf,:) = 0;
+    pTemp(vrod,:) = 0;
+    pTemp = pTemp(any(pTemp,2),:);
+
+    uTemp = U(:,1);
+    uTemp(vsurf,:) = 0;
+    uTemp(vrod,:) = 0;
+    uTemp = uTemp(any(uTemp,2),:);
+
+    F = scatteredInterpolant(pTemp,uTemp,'natural','none');
+    xint = -1:0.05:1;
+    yint = -1:0.05:1;
+    zint = 0:0.05:1;
+    [x,y,z] = meshgrid(xint,yint,zint);
+    V = F(x,y,z);
+    contourslice(x,y,z,V,[-0.5 0 0.5],[],[])
+end
