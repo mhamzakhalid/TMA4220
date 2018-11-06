2c2dc23838b67f4c7e3dda2947ad8ca5df2d1be3
sindre <sindre.grostad@icloud.com>
2018-11-06 11:41:52 +0100

    Added physical constants

---
+++ Project2/getPhysicalConstant.m
@@ -0,0 +1,17 @@
+function alpha = getPhysicalConstant(elementType)
+    switch elementType
+        case 'Aluminium'
+            k = 237;
+            rho = 2700;
+            cp = 910;
+            alpha = k / (rho*cp);
+        case 'Dough'
+            k = 0.386;
+            rho = 1100;
+            cp = 1900;
+            alpha = k / (rho*cp);
+        otherwise
+            error('Only Aluminium and Dough are supported')
+    end
+end
+
---
+++ Project2/testScript.m
@@ -0,0 +1,35 @@
+%%TestScript
+clear all 
+close all
+clc
+%% 3D quadrature:
+p1 = [0;0;0];
+p2 = [0;2;0];
+p3 = [0;0;2];
+p4 = [2;0;0];
+g = @(x,y,z) exp(x);
+f = @(x) 0.5*exp(x)*(x^2-6*x+10);
+analytic = f(2) - f(0); %Most likely wrong.
+N = [1 4 5];
+numerical = zeros(1,length(N));
+for i = 1:length(N)
+    numerical(i) = quadrature3D(p1,p2,p3,p4,N(i),g);
+end
+error = abs(numerical - analytic);
+figure
+semilogy(N,error,'*-')
+%% 
+clear all
+close all 
+clc
+p1 = [0;0;0];
+p2 = [0;2;0];
+p3 = [0;0;2];
+p4 = [2;0;0];
+p = [p1,p2,p3,p4]';
+a = [1,2,3,4];
+figure
+tetramesh(a,p)
+xlabel('x')
+ylabel('y')
+zlabel('z')
