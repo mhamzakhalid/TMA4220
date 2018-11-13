f5cae01369334a3f561d87c4cd514657284d9ea6
sindre <sindre.grostad@icloud.com>
2018-11-13 09:04:37 +0100

    Added some plots

--- Project2/contourSlice.m
+++ Project2/contourSlice.m
@@ -1,28 +1,21 @@
-function contourSlice(P,U,vsurf, vrod)
+function contourSlice(P,U,vsurf, vrod, time)
+    pTemp = P;
+    uTemp = U(:,time);
+    F = scatteredInterpolant(pTemp,uTemp,'natural','none');
+    xint = -1:0.05:1;
+    yint = -1:0.05:1;
+    zint = 0:0.05:1;
+    [x,y,z] = meshgrid(xint,yint,zint);
+    V = F(x,y,z);
+    contourslice(x,y,z,V,[],[],[0.25 0.5 0.75],5)
+    view(3)
+    colorbar;
+    caxis([20 220])
+end
     % Removing boundary:
-    figure
-    for i = 1:30:4000
-        pTemp = P;
     %     pTemp(vsurf,:) = 0;
     %     pTemp(vrod,:) = 0;
     %     pTemp = pTemp(any(pTemp,2),:);
-
-        uTemp = U(:,i);
-    %     uTemp(vsurf,:) = 0;
+%     uTemp(vsurf,:) = 0;
     %     uTemp(vrod,:) = 0;
     %     uTemp = uTemp(any(uTemp,2),:);
-
-        F = scatteredInterpolant(pTemp,uTemp,'natural','none');
-        xint = -1:0.05:1;
-        yint = -1:0.05:1;
-        zint = 0:0.05:1;
-        [x,y,z] = meshgrid(xint,yint,zint);
-        V = F(x,y,z);
-%         figure
-        contourslice(x,y,z,V,[],[],[0.25 0.5 0.75])
-        view(3)
-        colorbar;
-        pause(0.01)
-        clf
-    end
-end
