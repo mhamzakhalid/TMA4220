5f0609ab6800f0d4ff84648c7388d2f161d35a41
sindre <sindre.grostad@icloud.com>
2018-11-18 11:23:26 +0100

    Added many plots

--- Project2/makePlots.m
+++ Project2/makePlots.m
@@ -14,13 +14,28 @@ for time = 1:stepSize:size(U,2)
     clf
 end
 
-%% Plot four different times
-time = [1, 20, 10, 200];
+%% Plot four different times Dough
+load('meshCakeVariablesDough2.mat')
+dt = tmax/size(U,2);
+time = [1, 200, 600, 1201];
 for i = 1:4
     h = figure;
     contourSlice(P, U, time(i))
-    str_title = sprintf('Time = %.0f seconds',abs(time(i)*dt-1));
+    str_title = sprintf('Dough at time = %.0f minutes',abs(time(i)*dt-1)/60);
     title(str_title)
+    set(gca,'fontsize', 16);
+end
+
+%% Plot four different times Aluminium
+load('meshCakeVariablesAluminium.mat')
+dt = tmax/size(U,2);
+time = [1, 20, 70, 250];
+for i = 1:4
+    h = figure;
+    contourSlice(P, U, time(i))
+    str_title = sprintf('Aluminium at time = %.0f seconds',abs(time(i)*dt-1));
+    title(str_title)
+    set(gca,'fontsize', 16);
 end
 
 %% Plot aluminium and Dough
@@ -29,15 +44,32 @@ savePrecisionAlu = savePrecision;
 tmaxAlu = tmax;
 UAlu = U;
 alphaAlu = getPhysicalConstant('Aluminium');
+dtAlu = tmaxAlu/size(UAlu,2);
 
-load('meshCakeVariablesDough.mat')
+load('meshCakeVariablesDough2.mat')
 savePrecisionDough = savePrecision;
 tmaxDough = tmax;
 UDough = U;
 alphaDough = getPhysicalConstant('Dough2');
+dtDough = tmaxDough/size(UDough,2);
 
 ratio = alphaAlu/alphaDough
 
+plotAtAluTime = 40;
+aluNum = floor(40/dtAlu);
+doughNum = floor(40*ratio/dtDough);
+
+figure
+contourSlice(P, UAlu, aluNum)
+str_title = sprintf('Aluminium at time = %.0f seconds with \\alpha = %.2e.',abs(aluNum*dtAlu-1),alphaAlu);
+title(str_title);
+set(gca,'fontsize', 16);
+
+figure
+contourSlice(P, UDough, doughNum)
+str_title = sprintf('Dough at time = %.0f seconds with \\alpha = %.2e.',abs(doughNum*dtDough-1),alphaDough);
+title(str_title);
+set(gca,'fontsize', 16);
 
 %%
 figure
